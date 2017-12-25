cc35b6477f74b4a5a597bf951a3d7bb8ea1457e7
Kip Cole <kipcole9@gmail.com>
2017-12-26 10:24:29 +1100

    Add date to logger output

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,5 @@ config :logger,
   backends: [{LoggerFileBackend, :log}]
 
 config :logger, :log,
-  path: System.get_env("POWER_STATUS_LOG_PATH")
+  path: System.get_env("POWER_STATUS_LOG_PATH"),
+  format: "\n$date $time $metadata[$level] $levelpad$message\n"
